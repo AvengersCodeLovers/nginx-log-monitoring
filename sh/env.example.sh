f046106dbd34492aa59a39411dce5d2ad3e5314d
trungquan17 <quanlightning17@gmail.com>
2019-06-12 09:49:30 +0700

    [1.0.0] Add watching log for mysql errors.

--- sh/env.example.sh
+++ sh/env.example.sh
@@ -1,8 +1,11 @@
-export TARGET_DIRECTORY=***
-export TARGET_ENV=***
-export TARGET_MANAGERS=***
+#!/bin/bash
+### This shell script is for setting environmnent variables
+export NGINX_LOG_DIRECTORY=***
+export MYSQL_LOG_DIRECTORY=***
+
+export ENV=***
+export MANAGERS=***
 
 export CW_ROOM_ID=***
 export CW_API_URL=***
 export CW_BOT_TOKEN=***
-
